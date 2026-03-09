00379cc6daef589bd4acd5ea451b8884858749db
koumoto <koumoto@aites.co.jp>
2026-03-09 15:09:22 +0900

    ・clsCodeLengthSetting.vb の追加

---
+++ PRG/T.R.ZCommonClass/Function/clsCodeLengthSetting.vb
@@ -0,0 +1,21 @@
+﻿Public Class clsCodeLengthSetting
+  ''' <summary>
+  '''  得意先コード
+  ''' </summary>
+  Public Shared ReadOnly CUSTOMER_CODE_LENGTH As Integer = 6
+
+  ''' <summary>
+  '''  商品コード
+  ''' </summary>
+  Public Shared ReadOnly ITEM_CODE_LENGTH As Integer = 6
+
+  ''' <summary>
+  '''  直送先コード
+  ''' </summary>
+  Public Shared ReadOnly TYOKUSO_CODE_LENGTH As Integer = 6
+
+  ''' <summary>
+  '''  担当者コード
+  ''' </summary>
+  Public Shared ReadOnly TANTO_CODE_LENGTH As Integer = 6
+End Class
